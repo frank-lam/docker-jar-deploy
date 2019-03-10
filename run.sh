0cb97747ab136a204bccc09c9c0cc7c066c7436a
Frank Lin <frank_lin@whu.edu.cn>
2019-03-10 13:13:21 +0800

    update run.sh project_path

--- run.sh
+++ run.sh
@@ -1,24 +1,20 @@
 # 配置
-
 ## 当前部分项目目录(绝对路径)
-deploy_path='/var/www/ai-cloud-producer'
-
-## 代码项目目录路径
-project_path='docker-springboot-demo'
-
+deploy_path='/usr/local/docker/ai-cloud-producer'
+## 代码项目目录路径(相对路径)
+project_path='ai-cloud/model-producer'
 # 更新代码
-cd $project_path/ ; git pull ;cd $deploy_path
-
+cd $project_path
+git pull
+cd $deploy_path
 # 打包程序
-rm target/*.jar -f
-cd $project_name/
+rm target/*.jar -f ;
+cd $project_path ;
 mvn clean package
 cp target/*.jar $deploy_path/target/ -f
 cd $deploy_path
-
 # 启动项目
 docker-compose up -d --build --force-recreate
-
 # 构建镜像
 # sh build-image.sh
 # docker-compose up -d
